0fc64a1398b0e84873f16df2804db94b36a11a8a
Mauricio vasquez villanueva <mauriciovasquezvillanueva@gmail.com>
2023-03-23 21:49:18 -0500

    Menge branch 'main' of https://github.com/turcompu2000/personas-app.git into main

---
+++ resources/views/comunas/index.blade.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <div class="container">
+      <h1>Listado de comunas</h1>
+      <a href="[{ route('comunas.create') }]" class="btn btn-seccess">Add</a>
+    <table class="table">
+   <thead>
+     <tr>
+     <th scope="col">Code</th>
+     <th scope="col">Commune</th>
+     <th scope="col">Municipality</th>
+     <th scope="col">Actions</th>
+      </tr>
+   </thead>
+    <tbody>
+      @foreach ($comunas as $comuna)
+        <tr>
+          <th scope="row">{{$comuna->comu_codi}}</th>
+          <td>{{ $comuna->comu_nomb }}</td>
+          <td>{{ $comuna->muni_codi }}</td>
+          <td><span>Actions</td></span>
+         </tr>
+       @endforeach
+    </tbody>
+ </table>
+</div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  </body>
+</html>
+
+
+
